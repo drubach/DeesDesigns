d263a40f0bcc0f51774812807c318c83c6f7ac19
drubach <daniel.c.rubach@gmail.com>
2022-03-29 17:06:05 -0400

    initial commit

---
+++ dees_designs/asgi.py
@@ -0,0 +1,16 @@
+***REMOVED***
+ASGI config for dees_designs project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+***REMOVED***
+https://docs.djangoproject.com/en/3.2/howto/deployment/asgi/
+***REMOVED***
+
+***REMOVED***
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'dees_designs.settings')
+
+application = get_asgi_application()
---
+++ dees_designs/wsgi.py
@@ -0,0 +1,16 @@
+***REMOVED***
+WSGI config for dees_designs project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+***REMOVED***
+https://docs.djangoproject.com/en/3.2/howto/deployment/wsgi/
+***REMOVED***
+
+***REMOVED***
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'dees_designs.settings')
+
+application = get_wsgi_application()

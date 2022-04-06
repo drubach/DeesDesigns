0fcea4afd64ee8264f24c800f38a4c638137c48b
drubach <daniel.c.rubach@gmail.com>
2022-04-06 17:57:50 -0400

    customized admin for projects.

--- projects/admin.py
+++ projects/admin.py
@@ -3,5 +3,22 @@ from django.contrib import admin
 from .models import Project, Type
 
 # Register your models here.
-admin.site.register(Project)
-admin.site.register(Type)
+
+class ProjectAdmin(admin.ModelAdmin):
+    list_display = (
+        'user',
+        'type',
+        'completed',
+        'paid',
+        'image',
+    )
+
+    ordering = ('user', 'type', 'completed', 'paid')
+class TypeAdmin(admin.ModelAdmin):
+    list_display = (
+        'friendly_name',
+        'name',
+    )
+
+admin.site.register(Project, ProjectAdmin)
+admin.site.register(Type, TypeAdmin)

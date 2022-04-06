bd57c0f41a6fc0f84b171c5a1948c6b97674e306
drubach <daniel.c.rubach@gmail.com>
2022-04-06 16:25:18 -0400

    added projects app, models and loaded data.

--- projects/admin.py
+++ projects/admin.py
@@ -1,3 +1,7 @@
+""" Add models for Projects app. """
 from django.contrib import admin
+from .models import Project, Type
 
 # Register your models here.
+admin.site.register(Project)
+admin.site.register(Type)
--- projects/models.py
+++ projects/models.py
@@ -1,3 +1,32 @@
+""" Models for Projects App """
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Type(models.Model):
+    """ db Model for Types of projects """
+    name = models.CharField(max_length=254)
+    friendly_name = models.CharField(max_length=254, null=True, blank=True)
+
+    def __str__(self):
+        """ Function to return type name. """
+        return str(self.name)
+
+    def get_friendly_name(self):
+        """ Function to return type friendly name. """
+        return self.friendly_name
+
+class Project(models.Model):
+    """ db Model for project info """
+    user = models.ForeignKey(User, on_delete=models.CASCADE, default=1)
+    project_name = models.CharField(max_length=254)
+    description = models.TextField()
+    cost = models.DecimalField(max_digits=8, decimal_places=0)
+    type = models.ForeignKey('Type', null=True, blank=True, on_delete=models.SET_NULL)
+    completed = models.IntegerField(null=True, blank=True)
+    paid = models.IntegerField(null=True, blank=True)
+    image_url = models.URLField(max_length=1024, null=True, blank=True)
+    image = models.ImageField(null=True, blank=True)
+
+    def __str__(self):
+        """ Function to return project name. """
+        return str(self.project_name)

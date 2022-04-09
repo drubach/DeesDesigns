e5ac7ebdb654436e210359ed353be7c8b32cfc9d
drubach <daniel.c.rubach@gmail.com>
2022-04-09 11:57:24 -0400

    Styled project details page.

--- projects/views.py
+++ projects/views.py
@@ -1,11 +1,10 @@
 """ Views - Projects. """
-
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404, render
 from .models import Project
 
 
 def all_projects(request):
-    """ A view to go to view all Projects page. """
+    """ A view for all Projects page. """
 
     projects = Project.objects.all()
 
@@ -14,3 +13,16 @@ def all_projects(request):
     }
 
     return render(request, 'projects/projects.html', context)
+
+
+def project_detail(request, project_id):
+    """ A view to get project detail page. """
+
+    project = get_object_or_404(Project, pk=project_id)
+
+    context = {
+        'page': 'projects',
+        'project': project,
+    }
+
+    return render(request, 'projects/project_detail.html', context)

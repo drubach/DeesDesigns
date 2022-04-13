5c0565b565fb896173b0617ffcdf59b11f33661b
drubach <daniel.c.rubach@gmail.com>
2022-04-13 19:55:28 -0400

    Added AWS S3 storage code and custom_storages file.

---
+++ custom_storages.py
@@ -0,0 +1,12 @@
+""" Storage location setup for AWS. """
+from django.conf import settings
+from storages.backends.s3boto3 import S3Boto3Storage
+
+class StaticStorages(S3Boto3Storage):
+    """ Setting static file location. """
+    location = settings.STATICFILES_LOCATION
+
+
+class MediaStorages(S3Boto3Storage):
+    """ Setting media file location. """
+    location = settings.MEDIAFILES_LOCATION
--- dees_designs/settings.py
+++ dees_designs/settings.py
@@ -52,6 +52,10 @@ INSTALLED_APPS = [
     'allauth.socialaccount', # Installed from Django allauth doc site.
     'home',
     'projects',
+
+    # Other
+    'crispy_forms',
+    'storages'
 ]
 
 MIDDLEWARE = [
@@ -156,6 +160,24 @@ STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
 
 MEDIA_URL = 'media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
+if 'USE_AWS' in os.environ:
+    # Bucket Config
+    AWS_STORAGE_BUCKET_NAME = 'dees-designs'
+    AWS_S3_REGION_NAME = 'us-east-1'
+    AWS_ACCESS_KEY_ID = os.environ.get('AWS_ACCESS_KEY_ID')
+    AWS_SECRET_ACCESS_KEY = os.environ.get('AWS_SECRET_ACCESS_KEY')
+    AWS_S3_CUSTOM_DOMAIN = f'{AWS_STORAGE_BUCKET_NAME}.s3.amazon.com'
+
+    # Static & Media files
+    STATICFILES_STORAGE = 'custom_storages.StaticStorage'
+    STATICFILES_LOCATION = 'static'
+    DEFAULT_FILE_STORAGE = 'custom_storages.MediaStorage'
+    MEDIAFILES_LOCATION = 'media'
+
+    # Override static & media URLs in production.
+    STATIC_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/{STATICFILES_LOCATION}/'
+    MEDIA_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/{MEDIAFILES_LOCATION}/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field

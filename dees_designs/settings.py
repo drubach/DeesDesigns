eb08da8d2f51239e0790e175f50f863e7f5dbe2d
drubach <daniel.c.rubach@gmail.com>
2022-04-13 21:47:47 -0400

    fixed custom s3 domain name.

--- dees_designs/settings.py
+++ dees_designs/settings.py
@@ -173,7 +173,7 @@ if 'USE_AWS' in os.environ:
     AWS_S3_REGION_NAME = 'us-east-1'
     AWS_ACCESS_KEY_ID = os.environ.get('AWS_ACCESS_KEY_ID')
     AWS_SECRET_ACCESS_KEY = os.environ.get('AWS_SECRET_ACCESS_KEY')
-    AWS_S3_CUSTOM_DOMAIN = f'{AWS_STORAGE_BUCKET_NAME}.s3.amazon.com'
+    AWS_S3_CUSTOM_DOMAIN = f'{AWS_STORAGE_BUCKET_NAME}.s3.amazonaws.com'
 
     # Static & Media files
     STATICFILES_STORAGE = 'custom_storages.StaticStorage'

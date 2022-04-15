3986a0db832653757951e43ef0d26f9ef5390da6
drubach <daniel.c.rubach@gmail.com>
2022-04-15 12:24:20 -0400

    added static url & media url to if statement.

--- custom_storages.py
+++ custom_storages.py
@@ -4,9 +4,11 @@ from storages.backends.s3boto3 import S3Boto3Storage
 
 class StaticStorage(S3Boto3Storage):
     """ Setting static file location. """
+    #pylint: disable=W0223
     location = settings.STATICFILES_LOCATION
 
 
 class MediaStorage(S3Boto3Storage):
     """ Setting media file location. """
+    #pylint: disable=W0223
     location = settings.MEDIAFILES_LOCATION
--- dees_designs/settings.py
+++ dees_designs/settings.py
@@ -156,12 +156,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 
-STATIC_URL = '/static/'
-STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
-
-MEDIA_URL = '/media/'
-MEDIA_ROOT = (os.path.join(BASE_DIR, 'media'),)
-
 if 'USE_AWS' in os.environ:
     # Cache control
     AWS_S3_OBJECT_PARAMETERS = {
@@ -185,6 +179,12 @@ if 'USE_AWS' in os.environ:
     # Override static & media URLs in production.
     STATIC_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/{STATICFILES_LOCATION}/'
     MEDIA_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/{MEDIAFILES_LOCATION}/'
+else:
+    STATIC_URL = '/static/'
+    STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
+
+    MEDIA_URL = '/media/'
+    MEDIA_ROOT = (os.path.join(BASE_DIR, 'media'),)
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field

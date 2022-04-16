86748f1688b11cc9975a2768c66bbd8166decdf3
drubach <daniel.c.rubach@gmail.com>
2022-04-16 12:37:31 -0400

    fixed some static file source code.

--- dees_designs/settings.py
+++ dees_designs/settings.py
@@ -159,8 +159,8 @@ USE_TZ = True
 STATIC_URL = '/static/'
 STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
 
-MEDIA_URL = '/media/'
-MEDIA_ROOT = (os.path.join(BASE_DIR, 'media'),)
+MEDIA_URL = '../media/'
+MEDIA_ROOT = (os.path.join(BASE_DIR, '../media/'),)
 
 SITE_ID = 1 # Required by allauth
 

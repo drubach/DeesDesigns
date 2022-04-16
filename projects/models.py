3de63f24baedb136a96ae4698cd7f6cb03ddd30b
drubach <daniel.c.rubach@gmail.com>
2022-04-16 16:14:25 -0400

    changes to fix purecounter.

--- projects/models.py
+++ projects/models.py
@@ -27,7 +27,7 @@ class Project(models.Model):
     type = models.ForeignKey('Type', null=True, blank=True, on_delete=models.SET_NULL)
     completed = models.IntegerField(null=True, blank=True)
     paid = models.IntegerField(null=True, blank=True)
-    image_url = models.URLField(max_length=1024, null=True, blank=True)
+    image_url = models.URLField(max_length=2048, null=True, blank=True)
     image = models.ImageField(null=True, blank=True)
 
     def __str__(self):

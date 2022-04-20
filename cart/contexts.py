eb20a48de63cd785407c13f3f19cf80e7894707a
drubach <daniel.c.rubach@gmail.com>
2022-04-20 14:12:25 -0400

    Updated context with decimal limitation for currency transactions.

--- cart/contexts.py
+++ cart/contexts.py
@@ -1,4 +1,5 @@
 """ Context processor to make cart contents available across site. """
+from decimal import Decimal
 
 def cart_contents(request):
     """ Cart contents set up. """
@@ -7,10 +8,13 @@ def cart_contents(request):
     total = 0.00
     project_count = 0.00
 
+    grand_total = Decimal(total)
+
     context = {
         'cart_items' : cart_items,
         'total' : total,
         'project_count' : project_count,
+        'grand_total' : grand_total,
     }
 
     return context

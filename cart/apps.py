8f3458c9a42346601cda164937066ed040cf48dc
drubach <daniel.c.rubach@gmail.com>
2022-04-21 15:43:03 -0400

    Added forms, admin & signals for checkout.

--- cart/apps.py
+++ cart/apps.py
@@ -1,6 +1,10 @@
+""" Cart app. """
 from django.apps import AppConfig
 
 
 class CartConfig(AppConfig):
+    """
+    Cart app config.
+    """
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'cart'
--- checkout/apps.py
+++ checkout/apps.py
@@ -1,6 +1,16 @@
+""" Checkout app. """
 from django.apps import AppConfig
 
 
 class CheckoutConfig(AppConfig):
+    """
+    Configure checkout app.
+    """
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'checkout'
+
+    def ready(self):
+        """
+        Ensuring ability to update or delete order lineitems.
+        """
+        import checkout.signals

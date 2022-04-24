47c335913581a087a91fbe404392b04fc2af5102
drubach <daniel.c.rubach@gmail.com>
2022-04-23 21:14:54 -0400

    Updated usercart model.

--- cart/models.py
+++ cart/models.py
@@ -7,8 +7,9 @@ from django.contrib.auth.models import User
 class UserCart(models.Model):
     """ Basic cart model. """
     user = models.OneToOneField(User, on_delete=models.CASCADE)
-    cart = []
     grand_total = 0
+    cart = []
+    
 
     def __str__(self):
         return str(self.user)

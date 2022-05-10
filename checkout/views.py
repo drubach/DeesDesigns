68304e17d1882f646e4d3f08bef04469fa5dd217
drubach <daniel.c.rubach@gmail.com>
2022-05-09 22:20:20 -0400

    Rolling back stripe changes.

--- checkout/views.py
+++ checkout/views.py
@@ -1,25 +1,25 @@
-"""" Checkout views. """
-from django.contrib import messages
-from django.shortcuts import render, reverse, redirect
+# """" Checkout views. """
+# from django.contrib import messages
+# from django.shortcuts import render, reverse, redirect
 
-from .forms import OrderForm
+# from .forms import OrderForm
 
 
-def checkout(request):
-    """
-    Checkout view to render template.
-    """
-    cart = request.session.get('cart', {})
-    if not cart:
-        messages.error(request, "There's nothing in your cart at the moment.")
-        return redirect(reverse('home'))
+# def checkout(request):
+#     """
+#     Checkout view to render template.
+#     """
+#     cart = request.session.get('cart', {})
+#     if not cart:
+#         messages.error(request, "There's nothing in your cart at the moment.")
+#         return redirect(reverse('home'))
 
-    order_form = OrderForm()
-    template = 'checkout/checkout.html'
-    context = {
-        'order_form': order_form,
-        'stripe_public_key': 'pk_test_51KfTaaCzHgmfLydR4JJgW3DHFNkWSfaJx9mcP8pQiOK9KXtY6dWNY3qOi3TRZFzctoOeX6gSnsXVNce6xtrSe3Vx00A0CYz7D1',
-        'client_secret': 'test client secret',
-    }
+#     order_form = OrderForm()
+#     template = 'checkout/checkout.html'
+#     context = {
+#         'order_form': order_form,
+#         'stripe_public_key': 'pk_test_51KfTaaCzHgmfLydR4JJgW3DHFNkWSfaJx9mcP8pQiOK9KXtY6dWNY3qOi3TRZFzctoOeX6gSnsXVNce6xtrSe3Vx00A0CYz7D1',
+#         'client_secret': 'test client secret',
+#     }
 
-    return render(request, template, context)
+#     return render(request, template, context)
